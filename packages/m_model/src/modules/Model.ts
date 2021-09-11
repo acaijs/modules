5e07c309a5692107f79f1005dc89fb8f334716e0
Rafael <rafael.chaves98@hotmail.com>
2021-09-11 16:57:24 -0300

    v0.8.0-beta.7

--- packages/m_model/src/modules/Model.ts
+++ packages/m_model/src/modules/Model.ts
@@ -261,7 +261,7 @@ export default class Model {
 		}
 
 		// update fields
-		const updatedFields = await this.query().table($table).where($primary, id).first() as any
+		const updatedFields = await query().table($table).where($primary, id).first() as any
 		if (updatedFields) this.fill(updatedFields)
 	}
 
